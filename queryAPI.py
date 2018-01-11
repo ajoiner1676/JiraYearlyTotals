5a1029381785a75cbaf45cfbf57add2827803234
Allen Joiner / Project Manager <ajoiner@PMWS140.CAMCOM>
2018-01-11 09:44:23 -0600

    updated gitignore and completed queryAPI.py

--- queryAPI.py
+++ queryAPI.py
@@ -1,17 +1,30 @@
-import requests
+import sys
 import time
+import requests
 
-def yearly_total_req(issueType, year):
+def yearly_total_req(issueType, wantedYr):
+    #Get current year
+    wantedYr = int(wantedYr)
     yrDiff = time.localtime(time.time()).tm_year
-    if (year < yrDiff):
-        yrDiff = str(year - yrDiff)
+    if (wantedYr < yrDiff):
+        #Calculate year offset for query
+        yrDiff = str(wantedYr - yrDiff)
+    #Compile to query to return
     query = 'Type%20%3D%20' + issueType + '%20And%20Status%20in%20(Resolved%2C%20Completed)%20AND%20resolutiondate%20%3E%3D%20startOfYear(' + yrDiff + ')%20AND%20resolutiondate%20%3C%3DendOfYear(' + yrDiff + ')'
     r = 'http://camgian.atlassian.net/rest/api/latest/search?jql=' + query
     return r
 
-#auth = ',\'ajoiner\', \'Lis@1676\''
-resp = requests.get(yearly_total_req('Bug', 2017), auth=('ajoiner', 'Lis@1676'))
-print(resp.url)
-print(resp.status_code)
-ans = resp.json()
-print(ans['total'])
+#Get username, password, and year from cmd line
+usrName = sys.argv[1]
+passWrd = sys.argv[2]
+year = sys.argv[3]
+
+issues = ['Story', '"New Feature"', 'Task', 'Improvement', 'Bug']
+
+for x in issues:
+    #Submit request to Jira API store in resp
+    resp = requests.get(yearly_total_req(x, year), auth=(usrName, passWrd))
+    #print(resp.status_code)
+    #store response json in ans to access json structure
+    ans = resp.json()
+    print('Number of {} issues for {}: {}'.format(x, year, ans['total']))
